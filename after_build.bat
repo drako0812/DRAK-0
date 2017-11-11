7a749fd8ccce440d2d49f4bcf37390534a2c060c
Drako0812 <drako0812@gmail.com>
2017-11-11 08:06:56 -0600

    more fixing

--- after_build.bat
+++ after_build.bat
@@ -1,10 +1,9 @@
-@echo off
-echo +++ Running after_build.bat +++
+@echo +++ Running after_build.bat +++
 if "%proj_platform%"=="x86" goto copy
 goto quit
 
 :copy
-mkdir "C:/projects/DRAK-0/%prof_platform%/%proj_configuration%"
-xcopy "C:/projects/DRAK-0/%proj_configuration%/" "C:/projects/DRAK-0/%proj_platform%/%proj_configuration%/" /s /e /y
+mkdir "C:\projects\DRAK-0\x86\%proj_configuration%"
+xcopy "C:\projects\DRAK-0\%proj_configuration%" "C:\projects\DRAK-0\x86\%proj_configuration%" /s /e /y
 
 :exit
