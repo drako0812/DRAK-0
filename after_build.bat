915cafe27f4a6f47f7c9f0498e235e3cef156234
Drako0812 <drako0812@gmail.com>
2017-11-11 09:57:25 -0600

    builds should now not include intermediate files and maybe we now have a useful cache

--- after_build.bat
+++ after_build.bat
@@ -1,9 +1,19 @@
 @echo +++ Running after_build.bat +++
-if "%proj_platform%"=="x86" goto copy
-goto quit
+if "%proj_platform%"=="x86" goto copy else goto x64del
 
 :copy
 mkdir "C:\projects\DRAK-0\x86\%proj_configuration%"
 xcopy "C:\projects\DRAK-0\%proj_configuration%" "C:\projects\DRAK-0\x86\%proj_configuration%" /s /e /y
+del /S "C:\projects\DRAK-0\x86\%proj_configuration%\*.iobj"
+del /S "C:\projects\DRAK-0\x86\%proj_configuration%\*.ipdb"
+del /S "C:\projects\DRAK-0\x86\%proj_configuration%\*.ilk"
+del /S "C:\projects\DRAK-0\x86\%proj_configuration%\*.pdb"
+goto quit
+
+:x64del
+del /S "C:\projects\DRAK-0\x64\%proj_configuration%\*.iobj"
+del /S "C:\projects\DRAK-0\x64\%proj_configuration%\*.ipdb"
+del /S "C:\projects\DRAK-0\x64\%proj_configuration%\*.ilk"
+del /S "C:\projects\DRAK-0\x64\%proj_configuration%\*.pdb"
 
 :quit
