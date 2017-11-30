ebb36a189aa34ef63eefa7f62ee35557af113e57
Drako0812 <drako0812@gmail.com>
2017-11-29 22:07:20 -0600

    omg patches

--- install.bat
+++ install.bat
@@ -5,15 +5,15 @@ if "%proj_platform%"=="x86" ( goto BIT86 ) else ( if "%proj_platform%"=="x64" ( 
 
 :BIT86
 vcpkg install sfml:x86-windows
-%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
 goto END
 
 :BIT64
 vcpkg install sfml:x64-windows
-%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 goto END
 
 :END
 cd "C:\projects\drak-0\deps\luajit-2.0\src"
-.\msvcbuild.bat
+call .\msvcbuild.bat
 cd "C:\projects\drak-0"
